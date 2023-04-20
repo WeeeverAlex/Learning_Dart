315e48fa6c3089ef438ee3a1e94203891f3db8ac
WeeverAlex <awever4000@gmail.com>
2023-04-20 14:08:10 -0300

    commit dart

---
+++ Desktop/Flutter/variaveis_basicas_dart/test/variaveis_basicas_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:variaveis_basicas_dart/variaveis_basicas_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
